adcfae0b69522309319e29f47f44f0d04cd5ea1b
Adam Jodłowski <adam@jodlowski.net>
2012-06-17 20:46:25 +0200

    server apps

---
+++ node.js
@@ -0,0 +1,123 @@
+// MongoDB connection
+
+var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost:27017/nodejs', function() {
+
+  // clearing database
+  PostModel.remove({}, function(err, deleted) {});
+
+});
+
+// post schema
+
+var Schema = mongoose.Schema;
+
+var Post = new Schema({
+  author: String,
+  date: Date,
+  content: String
+});
+
+mongoose.model('Post', Post);
+PostModel = mongoose.model('Post');
+
+// Restify config
+
+var restify = require('restify');  
+var server = restify.createServer({
+  name: 'Node.js vs vert.x server'
+});
+server.use(restify.bodyParser());
+server.use(restify.queryParser());
+
+// don't send custom headers back
+
+restify.defaultResponseHeaders = null;
+
+// post route handler
+
+server.get('/post', function(req, res) {
+
+  var post = new PostModel(); 
+  post.author = randomString(16);
+  post.date = new Date();
+  post.content = randomString(160);
+
+  post.save(function (err) {
+
+    if (err) {
+      res.send(500);
+    } else {
+
+      PostModel.find().limit(100).execFind(function (err, data) {
+
+        if (err) {
+          res.send(500);
+        } else {
+
+          // a little bit extra work
+          fibonacci(30);
+
+          res.send(200, data);
+
+        }
+        
+      });
+
+    }
+
+  });
+
+});
+
+// hello route handler
+
+server.get('/hello', function(req, res) {
+
+  res.send(200, {message: 'hello'});
+
+});
+
+// render route handler
+
+server.get('/render', function(req, res) {
+
+  var response = randomString(10000);
+
+  res.send(200, {page: response});
+
+});
+
+
+// starting server
+
+server.listen(1337, function() {
+  console.log(server.name + ' is listening at ' + server.url);
+});
+
+// helper function for random string generation
+
+var randomString = function(_len) {
+
+  var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  var len = _len || 160;
+  var result = '';
+  var rand;
+  
+  for(var i = 0; i < len; i++) {
+    rand = Math.floor(Math.random()*(alphabet.length+1));
+    result += alphabet.substring(rand, rand + 1);
+  }
+    
+  return result;
+
+};
+
+// helper Fibonacci function
+
+var fibonacci = function(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
---
+++ vert.js
@@ -0,0 +1,189 @@
+// loading vertx shim
+
+load('vertx.js');
+
+// getting EventBus handler
+
+var eb = vertx.eventBus;
+
+// setting up MongoDB persistor
+
+var mongo = 'vertx.mongopersistor';
+
+vertx.deployVerticle('mongo-persistor', null, 1, function() {
+
+  // clearing database
+  eb.send(mongo, {action: 'delete', collection: 'vertx', matcher: {}});
+
+});
+
+// setting up server
+
+var server = vertx.createHttpServer();
+
+var routeMatcher = new vertx.RouteMatcher();
+
+// post route handler
+
+routeMatcher.get('/post', function(req, res) {
+  
+  var response = '';
+
+  var startDate = new Date();
+
+  // database access
+
+  eb.send(mongo, {action: 'save',
+                  collection: 'vertx',
+                  document: {
+                    author: randomString(16),
+                    date: new Date().toString(),
+                    content: randomString(160)
+                  }},
+
+  function(reply) {
+    
+    if (reply.status === 'ok') {
+      
+      eb.send(mongo, {action: 'find',
+                      collection: 'vertx',
+                      limit: 100,
+                      matcher: {}},
+
+      function(reply) {
+        
+        if (reply.status === 'ok') {
+          
+          response = JSON.stringify(reply.results);
+
+          // a little bit extra work
+          fibonacci(30);
+
+          var endDate = new Date();
+
+          req.response.putAllHeaders({
+            'Content-Type': 'application/json',
+            'Date': endDate.toString(),
+            'Connection': 'close',
+            'X-Response-Time': endDate - startDate,
+            'Server': 'vert.x vs Node.js server'
+          });
+
+          req.response.end(response);
+          req.response.close();
+          
+        } else {
+          
+          request.response.statusCode = 500;
+
+          var endDate = new Date();
+
+          req.response.putAllHeaders({
+            'Content-Type': 'application/json',
+            'Date': endDate.toString(),
+            'Connection': 'close',
+            'X-Response-Time': endDate - startDate,
+            'Server': 'vert.x vs Node.js server'
+          });
+
+          req.response.end(response);
+          req.response.close();
+
+        }
+
+      });
+
+    } else {
+      
+      request.response.statusCode = 500;
+
+      var endDate = new Date();
+
+      req.response.putAllHeaders({
+        'Content-Type': 'application/json',
+        'Date': endDate.toString(),
+        'Connection': 'close',
+        'X-Response-Time': endDate - startDate,
+        'Server': 'vert.x vs Node.js server'
+      });
+
+      req.response.end(response);
+      req.response.close();
+
+    }
+
+  });
+
+});
+
+// hello route handler
+
+routeMatcher.get('/hello', function(req, res) {
+
+  var startDate = new Date();
+
+  var endDate = new Date();
+
+  req.response.putAllHeaders({
+    'Content-Type': 'application/json',
+    'Date': endDate.toString(),
+    'Connection': 'close',
+    'X-Response-Time': endDate - startDate,
+    'Server': 'vert.x vs Node.js server'
+  });
+
+  req.response.end(JSON.stringify({message: 'hello'}));
+  req.response.close();
+
+});
+
+// render route handler
+
+routeMatcher.get('/render', function(req, res) {
+
+  var startDate = new Date();
+
+  var response = randomString(10000);
+
+  var endDate = new Date();
+
+  req.response.putAllHeaders({
+    'Content-Type': 'application/json',
+    'Date': endDate.toString(),
+    'Connection': 'close',
+    'X-Response-Time': endDate - startDate,
+    'Server': 'vert.x vs Node.js server'
+  });
+
+  req.response.end(JSON.stringify({page: response}));
+  req.response.close();
+
+});
+
+server.requestHandler(routeMatcher).listen(1338, 'localhost');
+
+// helper function for random string generation
+
+var randomString = function(_len) {
+
+  var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  var len = _len || 160;
+  var result = '';
+  var rand;
+  
+  for(var i = 0; i < len; i++) {
+    rand = Math.floor(Math.random()*(alphabet.length+1));
+    result += alphabet.substring(rand, rand + 1);
+  }
+    
+  return result;
+
+};
+
+// helper Fibonacci function
+
+var fibonacci = function(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
